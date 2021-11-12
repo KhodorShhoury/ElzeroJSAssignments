d4095fefccc463c1c833e4000a1c82419f24edaf
KhodorShhoury <khodorshhoury1@gmail.com>
2021-11-12 22:02:19 +0200

    add files

---
+++ js/64-70.js
@@ -0,0 +1,46 @@
+//1
+
+function getDetails(zName, zAge, zCountry) {
+     function namePattern(zName) {
+       return `Hello ${zName.split(" ")[0]} ${zName.split(" ")[1].slice(0,1).toUpperCase()}.`
+       // Osama Mohamed => Osama M.
+       // Ahmed ali => Ahmed A.
+     }
+     function ageWithMessage(zAge) {
+       return `Your Age Is ${zAge.slice(0,2)}`
+       // 38 Is My Age => Your Age Is 38
+       // 32 Is The Age => Your Age Is 32
+     }
+     function countryTwoLetters(zCountry) {
+      return `you live in ${zCountry.slice(0,2).toUpperCase()}`
+       // Egypt => You Live In EG
+       // Syria => You Live In SY
+     }
+     function fullDetails() {
+       return `${namePattern(zName)}, ${ageWithMessage(zAge)}, ${countryTwoLetters(zCountry)}`
+     }
+     return fullDetails(); // Do Not Edit This
+   }
+   
+   console.log(getDetails("Osama Mohamed", "38 Is My Age", "Egypt"));
+   // Hello Osama M., Your Age Is 38, You Live In EG
+   
+   console.log(getDetails("Ahmed ali", "32 Is The Age", "Syria"));
+   // Hello Ahmed A., Your Age Is 32, You Live In SY
+
+
+   //2
+   let itsMe = ()=> `Iam A Normal Function`;
+
+   console.log(itsMe()); // Iam A Normal Function
+
+   //second function
+
+   let urlCreate = (protocol,Web,tld) =>`${protocol}://www.${Web}.${tld}`;
+
+   console.log(urlCreate("https", "elzero", "org")); // https://www.elzero.org
+
+
+   //3
+
+   
