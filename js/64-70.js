c4bbac069402e1469fc6bad7928c4902778af7ab
KhodorShhoury <khodorshhoury1@gmail.com>
2021-11-13 12:52:55 +0200

    add files

--- js/64-70.js
+++ js/64-70.js
@@ -43,4 +43,28 @@ function getDetails(zName, zAge, zCountry) {
 
    //3
 
-   
+
+   //4
+
+   function specialMix(...data) {
+     let result=0;
+    for(let i = 0;i<data.length;i++){
+      if(isNaN(parseInt(data[i]))){
+        ""
+      }else{
+        result += parseInt(data[i])
+      }
+      
+    }
+    if (result === 0) {
+      return `All is strings`;
+    }
+    return result
+  }
+
+  
+  
+  console.log(specialMix(10, 20, 30)); // 60
+  console.log(specialMix("10Test", "Testing", "20Cool")); // 30
+  console.log(specialMix("Testing", "10Testing", "40Cool")); // 50
+  console.log(specialMix("Test", "Cool", "Test")); // All Is Strings
