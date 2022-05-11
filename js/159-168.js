ea41f2ac60be50e16253465fd47665c5fbc55c05
Khodor <khodorshhoury1@gmail.com>
2022-05-11 23:42:51 +0300

    add flies

--- js/159-168.js
+++ js/159-168.js
@@ -1,9 +1,17 @@
 //1
 
 let dateNow = new Date();
-let myBirthday =  new Date("july 28, 98");
+let myBirthday =  new Date("Oct 25, 82");
 
 let dateDiff = dateNow - myBirthday;
 
 
-console.log(dateNow)
+console.log(`${(dateDiff / 1000).toFixed()} Seconds`);
+console.log(`${(dateDiff / 1000 / 60).toFixed()} Minutes`);
+console.log(`${(dateDiff / 1000 / 60 / 60).toFixed()} Hours`);
+console.log(`${(dateDiff / 1000 / 60 / 60 / 24).toFixed()} Days`);
+console.log(`${(dateDiff / 1000 / 60 / 60 / 24 / 30).toFixed()  } Months`);
+console.log(`${(dateDiff / 1000 / 60 / 60 / 24  / 365).toFixed()} Years`);
+
+
+//2
