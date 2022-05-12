0886b3622e7a95383c33795e8d770ada584d6f75
khodorshhoury1@gmail.com <khodorshhoury1@gmail.com>
2022-05-12 15:54:09 +0300

    add files

--- js/159-168.js
+++ js/159-168.js
@@ -14,4 +14,26 @@ console.log(`${(dateDiff / 1000 / 60 / 60 / 24 / 30).toFixed()  } Months`);
 console.log(`${(dateDiff / 1000 / 60 / 60 / 24  / 365).toFixed()} Years`);
 
 
+// Needed Output
+
+/*
+"1247939400 Seconds"
+"20798990 Minutes"
+"346650 Hours"
+"14444 Days"
+"481 Months"
+"40 Years"
+*/
 //2
+let dateEditted = new Date();
+
+dateEditted.setFullYear(1980);
+dateEditted.setHours(0);
+dateEditted.setMinutes(0)
+dateEditted.setSeconds(1);
+console.log(dateEditted);
+
+// Needed Output
+
+//"Tue Jan 01 1980 00:00:01 GMT+0200 (Eastern European Standard Time)"
+
